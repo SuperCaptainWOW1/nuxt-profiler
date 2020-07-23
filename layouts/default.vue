312aec523405457a760d4393d05b503dea91bba1
Vladimir Makhortov <makhortov.dev@gmail.com>
2020-07-24 00:27:53 +0300

    Added base styles to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,3 +7,17 @@
 <script>
 export default {}
 </script>
+
+<style>
+html {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
